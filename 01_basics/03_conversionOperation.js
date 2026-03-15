62ff101d31b70c69bf7a6ccd568e28b87de26c39
yadavaja <62647184+yadavaja@users.noreply.github.com>
2026-03-15 23:02:08 +0000

    add string()

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,3 +20,9 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
