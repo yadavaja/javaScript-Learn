a8e8d7a96de15590e9267eb19df54d13f9d3933e
yadavaja <62647184+yadavaja@users.noreply.github.com>
2026-03-15 22:24:45 +0000

    add dataTypes.js

---
+++ 01_basics/02_dataTyes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser so it wont work here
+
+console.log( 3
+    +
+    3
+     ) // it works but code resuability is not there so dont use such way
+
+     console.log("Ajay")
+
+
+     let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
